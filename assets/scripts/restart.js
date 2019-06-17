b92d7636566a6ae39ac0235c7097f345cab3235f
priestv <51874560+priestv@users.noreply.github.com>
2019-06-17 17:50:18 +0800

    end
    end

---
+++ assets/scripts/restart.js
@@ -0,0 +1,20 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+    toscene:function(){
+        cc.director.loadScene("game");
+
+    },
+    start () {
+
+    },
+
+    // update (dt) {},
+});
